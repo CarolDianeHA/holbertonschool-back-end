10614f9f4b68f4df1cde408a3cce690aac569b8c
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-06-20 22:28:07 -0400

    Export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""Script to convert info to a JSON format for a single user."""
+"""Gather information from API."""
 
 import json
 from requests import get
 from sys import argv
 
 if __name__ == "__main__":
-    # Give us the user id
     user_id = argv[1]
     url = 'https://jsonplaceholder.typicode.com/users'
     user_data = get(f'{url}?id={user_id}').json()
 
-    # Select the username from the information given.
     for item in user_data:
         username = item['username']
 
-    # Select todos per user, using a given user's id.
     todos = get(f'{url}/{user_id}/todos').json()
     data_list = []
     final_dictionary = {}
@@ -30,4 +27,4 @@ if __name__ == "__main__":
         data_list.append(dic)
     final_dictionary[user_id] = data_list
     with open(f'{user_id}.json', 'w') as f:
-        json.dump(final_dictionary, f, indent=2)
+        json.dump(final_dictionary, f)
