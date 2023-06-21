e29c4bb29f633bc011ce6c66ac393c3f4b4b31b2
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-06-20 20:44:55 -0400

    Export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -7,7 +7,6 @@ import sys
 
 
 if __name__ == '__main__':
-
     user_id = sys.argv[1]
     api_url = "https://jsonplaceholder.typicode.com"
     url = f"{api_url}/users/{user_id}/todos"
@@ -16,23 +15,20 @@ if __name__ == '__main__':
     info = data.json()
     users_names = requests.get(users)
     response = users_names.json()
-    undone = 0
-    done = 0
 
     for user in response:
         username = user['username']
 
     json_filename = f"{user_id}.json"
-    json_data = []
+    json_data = {user_id: []}
 
     for item in info:
         done = item['completed']
         task_title = item['title']
-        json_data.append({
-            "USER_ID": user_id,
-            "USERNAME": username,
-            "TASK_COMPLETED_STATUS": done,
-            "TASK_TITLE": task_title
+        json_data[user_id].append({
+            "task": task_title,
+            "completed": done,
+            "username": username
         })
 
     with open(json_filename, mode='w') as file:
