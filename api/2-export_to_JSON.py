0bff1922aa1c04732ba64144a1b5cf1bba346be6
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-06-20 22:01:52 -0400

    Export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,34 +1,33 @@
 #!/usr/bin/python3
-"""Gather information from API."""
+"""Script to convert info to a JSON format for a single user."""
 
 import json
-import requests
-import sys
+from requests import get
+from sys import argv
 
+if __name__ == "__main__":
+    # Give us the user id
+    user_id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users'
+    user_data = get(f'{url}?id={user_id}').json()
 
-if __name__ == '__main__':
+    # Select the username from the information given.
+    for item in user_data:
+        username = item['username']
 
-    emp_id = sys.argv[1]
-
-    api_url = "https://jsonplaceholder.typicode.com"
-
-    url = f"{api_url}/users/{emp_id}/todos"
-    resp = requests.get(url)
-    emp_tasks = resp.json()
-
-    url = f"{api_url}/users/{emp_id}"
-    resp = requests.get(url)
-    emp_info = resp.json()
-
-    emp_todos = [
-        {
-            'task': task.get('title'),
-            'completed': task.get('completed'),
-            'username': emp_info.get('username'),
+    # Select todos per user, using a given user's id.
+    todos = get(f'{url}/{user_id}/todos').json()
+    data_list = []
+    final_dictionary = {}
+    for todo in todos:
+        tasks = todo['title']
+        done = todo['completed']
+        dic = {
+            "task": tasks,
+            "completed": done,
+            "username": username
         }
-        for task in emp_tasks
-    ]
-    output_json = {emp_id: emp_todos}
-
-    with open(f"{emp_id}.json", mode="w") as f:
-        json.dump(output_json, f, indent=2)
+        data_list.append(dic)
+    final_dictionary[user_id] = data_list
+    with open(f'{user_id}.json', 'w') as f:
+        json.dump(final_dictionary, f, indent=2)
